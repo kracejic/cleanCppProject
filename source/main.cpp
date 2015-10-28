5e289f1fc99915c2740fadf05973e42e0edb13c9
kracejic <kracejic@gmail.com>
2015-10-28 21:11:35 +0100

    first

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+//g++ main.cpp  -std=c++11 -o a.exe && ./a.exe
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"hello world!"<<endl;
+    return 0;
+}
